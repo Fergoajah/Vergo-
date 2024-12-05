80fbcc867e4afc0a4af8e098c701ed6b1d3922b1
Fergoajah <fergofernanda13@gmail.com>
2024-12-06 02:33:53 +0700

    Login Page

--- views/login.php
+++ views/login.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Hash password input user dengan MD5
+    // hash password input user dengan MD5
     $hashed_password = md5($password);
 
     // Query untuk mengambil data user/admin dengan celah SQL Injection
