eae63ff2dcc979986df8cbd829d06b96e36b2091
Fergoajah <fergofernanda13@gmail.com>
2024-12-18 14:03:22 +0700

    Logic to load user

--- views/users_handler.php
+++ views/users_handler.php
@@ -2,22 +2,17 @@
 session_start();
 require 'koneksi.php';
 
-// Ambil semua pengguna yang tidak termasuk pengguna yang sedang login
 $username = $_SESSION['username'];
 $query = "SELECT username, role FROM users WHERE role = 'user' AND username != '$username'";
 
-// Menjalankan query dan mengambil hasilnya
 $result = $mysqli->query($query);
 
-// Menyiapkan array untuk menyimpan data pengguna
 $users = [];
 if ($result && $result->num_rows > 0) {
     while ($user = $result->fetch_assoc()) {
         $users[] = $user;
     }
 }
-
-// Mengembalikan hasil dalam format JSON
 echo json_encode($users);
 
 ?>
